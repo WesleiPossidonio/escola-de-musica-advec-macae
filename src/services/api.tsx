a9d159a36b8a5b539ce17ac03d961c73fcc4d1ab
Weslei Possidonio <weslei.possidonio@gmail.com>
2025-05-06 11:46:25 -0300

    update link api

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/',
+  baseURL: 'https://api-emam.vercel.app/',
   withCredentials: true,
 })
 
